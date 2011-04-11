706c3cdcaa7c71ef3a8c71079e02d5d7e7357217
Luis Batista <luisbatista@domdigital.com>
2011-04-11 10:15:47 +0100

    Versao 1.0

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html><head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>DDLabs - CENARIO 02</title>
+</head>
+<body>
+<?php
+function ShowIndexMessage($msgindex){ echo "<h1>$msgindex</h1>"; }
+ShowIndexMessage("Estamos no Index.php - v1.0");
+echo "<a href=\"msg.php\">Ir para msg.php</a>";
+?>
+</body></html>
---
+++ msg.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html><head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>DDLabs - CENARIO 02</title>
+</head>
+<body>
+<?php
+function ShowMessage($msg,$version){echo "<h3>$msg - $version</h3>";}
+ShowMessage("Estamos no Msg.php","v1.0");
+echo "<a href=\"\">Voltar para index.php</a>";
+?>
+</body></html>

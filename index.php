d0e7fc8fecbc1a72fa39240ad0b474cc5966e026
Luis Batista <luisbatista@domdigital.com>
2011-04-11 10:50:15 +0100

    update

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html><head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>DDLabs - CENARIO 01</title>
+<title>DDLabs - CENARIO 02</title>
 </head>
 <body>
 <?php
-function getVersion(){return "2.0";}
-function ShowIndexMessage($msgindex,$version){ echo "<strong>$msgindex - $version</strong>"; }
-ShowIndexMessage("Estamos no Index.php",getVersion());
+function ShowIndexMessage($msgindex){ echo "<strong>$msgindex</strong><br >"; }
+ShowIndexMessage("Estamos no Index.php - v2.0");
 echo "<a href=\"msg.php\">Ir para msg.php</a>";
 ?>
 </body></html>

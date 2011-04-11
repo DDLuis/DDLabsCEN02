49210a91429e3ba8ed2e923a11d02f66968ac14b
Luis Batista <luisbatista@domdigital.com>
2011-04-11 10:42:34 +0100

    Versao 2.0

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html><head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>DDLabs - CENARIO 02</title>
+<title>DDLabs - CENARIO 01</title>
 </head>
 <body>
 <?php
-function ShowIndexMessage($msgindex){ echo "<h1>$msgindex</h1>"; }
-ShowIndexMessage("Estamos no Index.php - v1.0");
+function getVersion(){return "2.0";}
+function ShowIndexMessage($msgindex,$version){ echo "<strong>$msgindex - $version</strong>"; }
+ShowIndexMessage("Estamos no Index.php",getVersion());
 echo "<a href=\"msg.php\">Ir para msg.php</a>";
 ?>
 </body></html>
